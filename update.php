0040c25b2a8badc0d1d5771d9e86d3097aba006a
leproso <xge3000@hotmail.com>
2018-04-08 12:20:18 -0400

    Update update.php

--- update.php
+++ update.php
@@ -1,17 +1,17 @@
 <?php
- include "db.php";
- echo 'hello';
- if(isset($_GET['update']))
- {
- $id=111;
- $name=$_GET['name'];
- $client=$_GET['client'];
- $active=$_GET['active'];
-  
- $q=mysqli_query($connection,"INSERT INTO `im_home` (`name`,`client`,`active`) VALUES ('$name','$client','$active' where `id`='$id'");
- if($q)
- echo "success";
- else
- echo "error";
- }
- ?>
+include "db.php";
+echo 'hello';
+if(isset($_GET['update']))
+{
+$id=111;
+$name=$_GET['name'];
+$client=$_GET['client'];
+$active=$_GET['active'];
+ 
+$q=mysqli_query($connection,"INSERT INTO `im_home` (`name`,`client`,`active`) VALUES ('$name','$client','$active' where `id`='$id'");
+if($q)
+echo "success";
+else
+echo "error";
+}
+?>
