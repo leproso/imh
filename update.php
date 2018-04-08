7b716d862cbee6d9d6de4f72d848209a6f21657c
leproso <xge3000@hotmail.com>
2018-04-08 12:29:09 -0400

    Update update.php

--- update.php
+++ update.php
@@ -7,7 +7,7 @@ $id=111;
 $name=$_GET['name'];
 $client=$_GET['client'];
 $active=$_GET['active'];
- 
+  
 $q=mysqli_query($connection,"INSERT INTO `im_home` (`name`,`client`,`active`) VALUES ('$name','$client','$active' where `id`='$id'");
 if($q)
 echo "success";
