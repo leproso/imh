876ce9eca4276d5116696b499191d1c48a6c4e5b
leproso <xge3000@hotmail.com>
2018-04-08 01:26:24 -0400

    Update insert.php

--- insert.php
+++ insert.php
@@ -5,7 +5,7 @@
 $name=$_POST['name'];
 $client=$_POST['client'];
 $active=$_POST['active'];
-$q=mysqli_query($con,"INSERT INTO `course_details` (`name`,`client`,`active`) VALUES ('$name','$client','$active')");
+$q=mysqli_query($con,"INSERT INTO `im_home` (`name`,`client`,`active`) VALUES ('$name','$client','$active')");
  if($q)
   echo "success";
  else
