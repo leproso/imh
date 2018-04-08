5c72798acc06b7cd74069164f2f3ffc775b8084a
leproso <xge3000@hotmail.com>
2018-04-08 01:25:18 -0400

    Create update.php

---
+++ update.php
@@ -0,0 +1,15 @@
+<?php
+ include "db.php";
+ if(isset($_POST['update']))
+ {
+ $id=$_POST['id'];
+ $name=$_POST['name'];
+ $client=$_POST['client'];
+ $active=$_POST['active'];
+ $q=mysqli_query($con,"INSERT INTO `course_details` (`name`,`client`,`active`) VALUES ('$name','$client','$active' where `id`='$id'");
+ if($q)
+ echo "success";
+ else
+ echo "error";
+ }
+ ?>
