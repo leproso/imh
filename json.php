50c7feb9e344679ea0a2d6ed5604286396b0fb9a
leproso <xge3000@hotmail.com>
2018-04-08 04:02:32 -0400

    Update json.php

--- json.php
+++ json.php
@@ -1,7 +1,7 @@
 <?php
 include "db.php";
 $data=array();
-$q=mysqli_query($con,"select * from `im_home`");
+$q=mysqli_query($connection,"select * from `im_home`");
 while ($row=mysqli_fetch_object($q)){
  $data[]=$row;
 }
