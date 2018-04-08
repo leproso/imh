47caf8dce77b44e7005c5c344a8f282b61c92945
leproso <xge3000@hotmail.com>
2018-04-08 11:40:02 -0400

    Update update.php

--- update.php
+++ update.php
@@ -1,11 +1,13 @@
 <?php
  include "db.php";
+ echo 'hello';
  if(isset($_POST['update']))
  {
  $id=$_POST['id'];
  $name=$_POST['name'];
  $client=$_POST['client'];
  $active=$_POST['active'];
+  
  echo $id + ' // ' + $name + ' // ' + $client + ' // ' + $active + ' // ' + $_POST['update'] + ' </br> ';
  $q=mysqli_query($connection,"INSERT INTO `im_home` (`name`,`client`,`active`) VALUES ('$name','$client','$active' where `id`='$id'");
  if($q)
