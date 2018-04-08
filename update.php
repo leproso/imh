4992e4a0c8908754a7a43133a9ebfa6ea6f30d7f
leproso <xge3000@hotmail.com>
2018-04-08 12:11:58 -0400

    Update update.php

--- update.php
+++ update.php
@@ -8,7 +8,7 @@
  $client=$_GET['client'];
  $active=$_GET['active'];
   
- echo $id + ' // ' + $name + ' // ' + $client + ' // ' + $active + ' // ' + $_GET['update'] + ' </br> ';
+ echo $id . ' // ' . $name . ' // ' . $client . ' // ' . $active . ' // ' . $_GET['update'] . ' </br> ';
  $q=mysqli_query($connection,"INSERT INTO `im_home` (`name`,`client`,`active`) VALUES ('$name','$client','$active' where `id`='$id'");
  if($q)
  echo "success";
